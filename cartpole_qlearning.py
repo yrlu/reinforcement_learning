03afd8607d138c9e692f253758983ffbc9b35c8f
Max Lu <luyiren@seas.upenn.edu>
2017-01-28 23:53:26 -0500

    formatting cartpole

--- cartpole_qlearning.py
+++ cartpole_qlearning.py
@@ -4,20 +4,21 @@ import qlearning_agent
 import numpy
 import matplotlib.pyplot as plt
 
-NUM_EPISODES=3000
+NUM_EPISODES = 3000
 N_BINS = [8, 8, 8, 8]
 MAX_STEPS = 200
 FAIL_PENALTY = -100
-EPSILON=0.5
-EPSILON_DECAY=0.99
-LEARNING_RATE=0.05
-DISCOUNT_FACTOR=0.9
+EPSILON = 0.5
+EPSILON_DECAY = 0.99
+LEARNING_RATE = 0.05
+DISCOUNT_FACTOR = 0.9
 
-RECORD=True
+RECORD = True
 
-MIN_VALUES = [-0.5,-2.0,-0.5,-3.0]
-MAX_VALUES = [0.5,2.0,0.5,3.0]
-BINS = [numpy.linspace(MIN_VALUES[i], MAX_VALUES[i], N_BINS[i]) for i in xrange(4)]
+MIN_VALUES = [-0.5, -2.0, -0.5, -3.0]
+MAX_VALUES = [0.5, 2.0, 0.5, 3.0]
+BINS = [numpy.linspace(MIN_VALUES[i], MAX_VALUES[i], N_BINS[i])
+        for i in xrange(4)]
 
 
 def discretize(obs):
@@ -27,7 +28,7 @@ def discretize(obs):
 def train(agent, env, history, num_episodes=NUM_EPISODES):
   for i in xrange(NUM_EPISODES):
     if i % 100:
-      print "Episode {}".format(i+1)
+      print "Episode {}".format(i + 1)
 
     obs = env.reset()
     cur_state = discretize(obs)
@@ -40,33 +41,33 @@ def train(agent, env, history, num_episodes=NUM_EPISODES):
       if done:
         reward = FAIL_PENALTY
         agent.learn(cur_state, action, next_state, reward, done)
-        print("Episode finished after {} timesteps".format(t+1))
-        history.append(t+1)
+        print("Episode finished after {} timesteps".format(t + 1))
+        history.append(t + 1)
         break
 
       agent.learn(cur_state, action, next_state, reward, done)
       cur_state = next_state
 
-      if t == MAX_STEPS-1:
-        history.append(t+1)
-        print("Episode finished after {} timesteps".format(t+1))
+      if t == MAX_STEPS - 1:
+        history.append(t + 1)
+        print("Episode finished after {} timesteps".format(t + 1))
 
   return agent, history
-
 
 
 env = gym.make('CartPole-v0')
 if RECORD:
   env = wrappers.Monitor(env, '/tmp/cartpole-experiment-1', force=True)
 
+
 def get_actions(state):
   return [0, 1]
 
-agent=qlearning_agent.QLearningAgent(get_actions, 
-                                epsilon=EPSILON, 
-                                alpha=LEARNING_RATE, 
-                                gamma=DISCOUNT_FACTOR, 
-                                epsilon_decay=EPSILON_DECAY)
+agent = qlearning_agent.QLearningAgent(get_actions,
+                                       epsilon=EPSILON,
+                                       alpha=LEARNING_RATE,
+                                       gamma=DISCOUNT_FACTOR,
+                                       epsilon_decay=EPSILON_DECAY)
 
 history = []
 
@@ -80,9 +81,8 @@ plt.plot(history)
 plt.ylabel('Rewards')
 plt.show()
 
-
-# gym.upload('/tmp/cartpole-experiment-1', api_key='sk_gw4ldruFRAc3gAhzCBxNw')
-
+# # Display:
+#
 # while True:
 #   obs = env.reset()
 #   cur_state = discretize(obs)
