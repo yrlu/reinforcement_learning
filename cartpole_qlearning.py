804c00306409e597f2799fab59f78555a53bcc37
Max Lu <luyiren@seas.upenn.edu>
2017-01-29 23:21:07 -0500

    update cartpole demo

--- cartpole_qlearning.py
+++ cartpole_qlearning.py
@@ -4,7 +4,7 @@ import qlearning_agent
 import numpy
 import matplotlib.pyplot as plt
 
-NUM_EPISODES = 3000
+NUM_EPISODES = 1000
 N_BINS = [8, 8, 8, 8]
 MAX_STEPS = 200
 FAIL_PENALTY = -100
@@ -49,52 +49,57 @@ def train(agent, env, history, num_episodes=NUM_EPISODES):
   return agent, history
 
 
-if __name__ == '__main__':
-  env = gym.make('CartPole-v0')
-  if RECORD:
-    env = wrappers.Monitor(env, '/tmp/cartpole-experiment-1', force=True)
-  def get_actions(state):
-    return [0, 1]
+env = gym.make('CartPole-v0')
+if RECORD:
+  env = wrappers.Monitor(env, '/tmp/cartpole-experiment-1', force=True)
+def get_actions(state):
+  return [0, 1]
 
-  agent = qlearning_agent.QLearningAgent(get_actions,
-                                       epsilon=EPSILON,
-                                       alpha=LEARNING_RATE,
-                                       gamma=DISCOUNT_FACTOR,
-                                       epsilon_decay=EPSILON_DECAY)
+agent = qlearning_agent.QLearningAgent(get_actions,
+                                     epsilon=EPSILON,
+                                     alpha=LEARNING_RATE,
+                                     gamma=DISCOUNT_FACTOR,
+                                     epsilon_decay=EPSILON_DECAY)
 
-  history = []
+history = []
 
-  agent, history = train(agent, env, history)
+agent, history = train(agent, env, history)
 
-  if RECORD:
-    env.monitor.close()
+if RECORD:
+  env.monitor.close()
+
+print agent.policy
+
+avg_reward = [numpy.mean(history[i*100:(i+1)*100]) for i in xrange(int(len(history)/100))]
+print avg_reward
+f_reward = plt.figure(1)
+plt.plot(numpy.linspace(0, len(history), len(avg_reward)), avg_reward)
+plt.ylabel('Rewards')
+f_reward.show()
+print 'press enter to continue'
+raw_input()
+plt.close()
 
 
-  avg_reward = [numpy.mean(history[i*100:(i+1)*100]) for i in xrange(int(len(history)/100))]
-  print avg_reward
-  plt.plot(numpy.linspace(0, len(history), len(avg_reward)), avg_reward)
-  plt.ylabel('Rewards')
-  plt.show()
+# Display:
+print 'press ctrl-c to stop'
+while True:
+  obs = env.reset()
+  cur_state = discretize(obs)
+  done = False
 
-  # # Display:
-
-  # while True:
-  #   obs = env.reset()
-  #   cur_state = discretize(obs)
-  #   done = False
-
-  #   t = 0
-  #   while not done:
-  #     env.render()
-  #     t = t+1
-  #     action = agent.get_action(cur_state)
-  #     observation, reward, done, info = env.step(action)
-  #     next_state = discretize(observation)
-  #     if done:
-  #       reward = FAIL_PENALTY
-  #       agent.learn(cur_state, action, next_state, reward, done)
-  #       print("Episode finished after {} timesteps".format(t+1))
-  #       history.append(t+1)
-  #       break
-  #     agent.learn(cur_state, action, next_state, reward, done)
-  #     cur_state = next_state
+  t = 0
+  while not done:
+    env.render()
+    t = t+1
+    action = agent.get_action(cur_state)
+    observation, reward, done, info = env.step(action)
+    next_state = discretize(observation)
+    if done:
+      reward = FAIL_PENALTY
+      agent.learn(cur_state, action, next_state, reward, done)
+      print("Episode finished after {} timesteps".format(t+1))
+      history.append(t+1)
+      break
+    agent.learn(cur_state, action, next_state, reward, done)
+    cur_state = next_state
