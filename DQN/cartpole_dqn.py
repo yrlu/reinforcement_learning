b8fb35e313049174b4a4dc6f5f07dffd9b396903
Max Lu <luyiren@seas.upenn.edu>
2017-02-19 17:46:42 -0500

    clean up dqn

--- DQN/cartpole_dqn.py
+++ DQN/cartpole_dqn.py
@@ -1,68 +1,71 @@
 import gym
-from gym import wrappers
+import numpy as np
+import sys
+import tensorflow as tf
 import dqn
-import numpy
+import exp_replay
+from exp_replay import Step
 import matplotlib.pyplot as plt
 
 
-NUM_EPISODES = 200
+DEVICE = sys.argv[1]
+NUM_EPISODES = int(sys.argv[2])
+ACTIONS = {0:0, 1:1}
 MAX_STEPS = 300
 FAIL_PENALTY = -100
 EPSILON = 1
 EPSILON_DECAY = 0.01
-END_EPSILON = 0.05
+END_EPSILON = 0.1
 LEARNING_RATE = 0.001
 DISCOUNT_FACTOR = 0.9
 BATCH_SIZE = 32
 MEM_SIZE = 1e4
+START_MEM = 1e2
+STATE_SIZE = [4]
+EPOCH_SIZE = 100
 
-RECORD = False
 
-def train(agent, env, history, num_episodes=NUM_EPISODES):
-  for i in xrange(num_episodes):
-    if i % 100:
-      print "Episode {}".format(i + 1)
+def train(agent, exprep, env):
+  for i in xrange(NUM_EPISODES):
     cur_state = env.reset()
-    episode = []
     for t in xrange(MAX_STEPS):
       action = agent.get_action(cur_state)
       next_state, reward, done, info = env.step(action)
       if done:
         reward = FAIL_PENALTY
-        episode.append([cur_state, action, next_state, reward, done])
-        print("Episode finished after {} timesteps".format(t + 1))
-        history.append(t + 1)
+        exprep.add_step(Step(cur_step=cur_state, action=action, next_step=next_state, reward=reward, done=done))
+        print("Episode {} finished after {} timesteps".format(i, t + 1))
+        yield t + 1 
         break
-      episode.append([cur_state, action, next_state, reward, done])
+      exprep.add_step(Step(cur_step=cur_state, action=action, next_step=next_state, reward=reward, done=done))
       cur_state = next_state
       if t == MAX_STEPS - 1:
-        history.append(t + 1)
-        print("Episode finished after {} timesteps".format(t + 1))
-    agent.learn(episode, 100)
-  return agent, history
+        print("Episode {} finished after {} timesteps".format(i, t + 1))
+        yield t + 1
+    agent.epsilon_decay()
+    agent.learn_epoch(exprep, EPOCH_SIZE)
+    print 'epsilon: {}'.format(agent.epsilon)
 
 
 env = gym.make('CartPole-v0')
-if RECORD:
-  env = wrappers.Monitor(env, '/tmp/cartpole-experiment-2', force=True)
-
-agent = dqn.DQNAgent(epsilon=EPSILON, epsilon_anneal=EPSILON_DECAY, end_epsilon=END_EPSILON, 
-      lr=LEARNING_RATE, gamma=DISCOUNT_FACTOR, batch_size=BATCH_SIZE, state_size=4, 
-      action_size=2, mem_size=MEM_SIZE, n_hidden_1=10, n_hidden_2=10)
+exprep = exp_replay.ExpReplay(mem_size=MEM_SIZE, start_mem=START_MEM, state_size=STATE_SIZE, kth=1, batch_size=BATCH_SIZE)
 
 
-history = []
-agent, history = train(agent, env, history)
-print history
+
+sess = tf.Session()
+with tf.device('/{}:0'.format(DEVICE)):
+  agent = dqn.DQNAgent(session=sess, epsilon=EPSILON, epsilon_anneal=EPSILON_DECAY, end_epsilon=END_EPSILON, 
+        lr=LEARNING_RATE, gamma=DISCOUNT_FACTOR, state_size=4, 
+        action_size=len(ACTIONS), n_hidden_1=10, n_hidden_2=10)
+sess.run(tf.initialize_all_variables())
+history = [e_length for e_length in train(agent, exprep, env)]
 
 
-if RECORD:
-  env.monitor.close()
-
-
-avg_reward = [numpy.mean(history[i*10:(i+1)*10]) for i in xrange(int(len(history)/10))]
+# plot
+import matplotlib.pyplot as plt
+avg_reward = [np.mean(history[i*10:(i+1)*10]) for i in xrange(int(len(history)/10))]
 f_reward = plt.figure(1)
-plt.plot(numpy.linspace(0, len(history), len(avg_reward)), avg_reward)
+plt.plot(np.linspace(0, len(history), len(avg_reward)), avg_reward)
 plt.ylabel('Episode length')
 plt.xlabel('Training episodes')
 f_reward.show()
@@ -77,17 +80,18 @@ while True:
   cur_state = env.reset()
   done = False
   t = 0
-  episode = []
   while not done:
     env.render()
     t = t+1
     action = agent.get_action(cur_state)
     next_state, reward, done, info = env.step(action)
-    if done:
-      reward = FAIL_PENALTY
-      print("Episode finished after {} timesteps".format(t+1))
-      history.append(t+1)
-      break
-    episode.append([cur_state, action, next_state, reward, done])
     cur_state = next_state
-  agent.learn(episode, 100)
+    if done:
+      print("Episode finished after {} timesteps".format(t+1))
+      break
+    
+
+
+
+
+
--- DQN/dqn.py
+++ DQN/dqn.py
@@ -1,7 +1,19 @@
+# Deep Q-learning agent with q-value approximation
+# Following paper: Playing Atari with Deep Reinforcement Learning
+#     https://www.cs.toronto.edu/~vmnih/docs/dqn.pdf
+#
+# ---
+# @author Yiren Lu
+# @email luyiren [at] seas [dot] upenn [dot] edu
+#
+# MIT License
+
+
 import gym
 import numpy as np
 import random
 import tensorflow as tf
+import tf_utils
 
 
 class DQNAgent():
@@ -10,17 +22,17 @@ class DQNAgent():
   """
 
   def __init__(self,
+    session,
     epsilon=0.5, 
     epsilon_anneal = 0.01,
     end_epsilon=0.1,
     lr=0.5, 
-    gamma=0.99, 
-    batch_size=64, 
+    gamma=0.99,
     state_size=4,
     action_size=2,
-    mem_size=1e4,
+    scope="dqn",
     n_hidden_1=20,
-    n_hidden_2=20
+    n_hidden_2=20,
     ):
     """
     args
@@ -29,94 +41,49 @@ class DQNAgent():
       end_epsilon       lowest exploration rate
       lr                learning rate
       gamma             discount factor
-      batch_size         training minibatch size per iteration
       state_size        network input size
       action_size       network output size
-      mem_size          max memory size
     """
     self.epsilon = epsilon
     self.epsilon_anneal = epsilon_anneal
     self.end_epsilon = end_epsilon
     self.lr = lr
     self.gamma = gamma
-    self.batch_size = batch_size
     self.state_size = state_size
     self.action_size = action_size
-    self.mem_size = mem_size
-    self.total_steps = 0
+    self.scope = scope
     self.n_hidden_1 = n_hidden_1
     self.n_hidden_2 = n_hidden_2
-
-    self.mem = []
-
-    self.sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-    self.global_step = tf.Variable(0, name='global_step', trainable=False)
-    with tf.device('/cpu:0'):
-      self._build_qnet()
-    self.sess.run(tf.global_variables_initializer())
-
+    self._build_qnet()
+    self.sess = session
 
   def _build_qnet(self):
     """
     Build q-network
     """
-    
-    # input state
-    self.state_input = tf.placeholder(tf.float32, [None, self.state_size])
-    # input action to generate output mask
-    self.action = tf.placeholder(tf.int32, [None])
-    # target_q = tf.add(reward + gamma * max(q(s,a)))
-    self.target_q = tf.placeholder(tf.float32, [None])
+    with tf.variable_scope(self.scope):
+      self.state_input = tf.placeholder(tf.float32, [None, self.state_size])
+      self.action = tf.placeholder(tf.int32, [None])
+      self.target_q = tf.placeholder(tf.float32, [None])
 
-    # 2 hidden layers
-    # network: [state_size] - [n_hidden_1] - [n_hidden_2] - [action_size]
+      fc1 = tf_utils.fc(self.state_input, n_output=self.n_hidden_1, activation_fn=tf.nn.relu)
+      fc2 = tf_utils.fc(fc1, n_output=self.n_hidden_2, activation_fn=tf.nn.relu)
+      self.q_values = tf_utils.fc(fc2, self.action_size, activation_fn=None)
 
-    n_hidden_1 = self.n_hidden_1
-    n_hidden_2 = self.n_hidden_2
+      action_mask = tf.one_hot(self.action, self.action_size, 1.0, 0.0)
+      q_value_pred = tf.reduce_sum(self.q_values * action_mask, 1)
 
-    self.weights = {
-      'h1': tf.Variable(tf.random_normal([self.state_size, n_hidden_1])),
-      'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-      'out': tf.Variable(tf.random_normal([n_hidden_2, self.action_size]))
-    }
+      self.loss = tf.reduce_mean(tf.square(tf.subtract(self.target_q, q_value_pred)))
+      self.optimizer = tf.train.AdamOptimizer(self.lr)
+      self.train_op = self.optimizer.minimize(self.loss, global_step=tf.contrib.framework.get_global_step())
 
-    self.biases = {
-      'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-      'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-      'out': tf.Variable(tf.random_normal([self.action_size]))
-    }
-
-    layer_1 = tf.add(tf.matmul(self.state_input, self.weights['h1']), self.biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
-
-    layer_2 = tf.add(tf.matmul(layer_1, self.weights['h2']), self.biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
-
-    self.q_values = tf.add(tf.matmul(layer_2, self.weights['out']), self.biases['out'])
-
-    action_mask = tf.one_hot(self.action, self.action_size, 1.0, 0.0)
-    # predicted q(s,a)
-    q_value_pred = tf.reduce_sum(self.q_values * action_mask, 1)
-
-    self.loss = tf.reduce_mean(tf.square(tf.subtract(self.target_q, q_value_pred)))
-    self.optimizer = tf.train.AdamOptimizer(self.lr)
-    self.train_op = self.optimizer.minimize(self.loss, global_step=self.global_step)
-
-
-  def get_value(self, s):
-    a = self.get_optimal_action(s)
-    return self.get_qvalue(s, a)
-
-
-  def get_qvalue(self, s, a):
-    q_values = self.sess.run(self.q_values, feed_dict={self.state_input: [s]})
-    return q_values[0][a]
-
+  def get_action_values(self, state, sess):
+    actions = sess.run(self.q_values, feed_dict={self.state_input: [state]})
+    return actions
 
   def get_optimal_action(self, state):
     actions = self.sess.run(self.q_values, feed_dict={self.state_input: [state]})
     return actions.argmax()
-
 
   def get_action(self, state):
     """
@@ -127,71 +94,53 @@ class DQNAgent():
     returns
       an action to take given the state
     """
-
     if np.random.random() < self.epsilon:
       # act randomly
       return np.random.randint(0, self.action_size)
     else:
       return self.get_optimal_action(state)
 
-
-  def _add_episode(self, episode):
-    """
-    Store episode to memory and check if it reaches the mem_size. 
-    If so, drop 20% of the oldest memory
-
-    args
-      episode       a list of (current state, action, next state, reward, done)
-    """
+  def epsilon_decay(self):    
     if self.epsilon > self.end_epsilon:
       self.epsilon = self.epsilon - self.epsilon_anneal
 
-    for step in episode:
-      self.mem.append(step)
-
-    while len(self.mem) > self.mem_size:
-      # If memory reaches limit, then drop 20% of the oldest memory
-      self.mem = self.mem[int(len(self.mem)/5):]
-
-
-  def learn(self, episode, train_steps):
+  def learn_epoch(self, exprep, num_steps):
     """
-    Deep Q-learing:
-      - Store episode to the memory
-      - Sample minibatch from transitions (last state, action, next state, reward, done) from memory
-      - Train q-network (s->{a}) by the sampled transitions
+    Deep Q-learing: train qnetwork for num_steps, for each step, sample a batch from exprep
 
-    args
-      episode       a list of (current state, action, next state, reward, done)
-      train_steps   number of training steps per calling learn()
+    Args
+      exprep:         experience replay
+      num_steps:      num of steps
     """
-    self._add_episode(episode)
+    for i in xrange(num_steps):
+      self.learn_batch(exprep.sample())
 
-    if len(self.mem) > self.batch_size:
-      
-      for i in xrange(train_steps):
-        self.total_steps = self.total_steps + 1
-        target_weights = self.sess.run(self.weights)
+  def learn_batch(self, batch_steps):
+    """
+    Deep Q-learing: train qnetwork with the input batch
+    Args
+      batch_steps:    a batch of sampled namedtuple Step, where Step.cur_step and 
+                      Step.next_step are of shape {self.state_size}
+      sess:           tf session
+    Returns 
+      batch loss (-1 if input is empty)
+    """
+    if len(batch_steps) == 0:
+      return -1
 
-        sampled_idx = np.random.choice(len(self.mem), self.batch_size)
-        samples = random.sample(self.mem, self.batch_size)
+    next_state_batch = [s.next_step for s in batch_steps]
+    q_values = self.sess.run(self.q_values, feed_dict={self.state_input: next_state_batch})
 
-        # s[2] is next state
-        q_values = self.sess.run(self.q_values, feed_dict={self.state_input: [s[2] for s in samples]})
-        max_q_values = q_values.max(axis=1)
-
-        # compute target q value
-        target_q = np.array([samples[i][3] + self.gamma*max_q_values[i]*(1-samples[i][4]) for i in xrange(len(samples))])
-        target_q = target_q.reshape([self.batch_size])
-
-        # minimize the TD-error
-        l, _, = self.sess.run([self.loss, self.train_op], feed_dict={
-                                                            self.state_input: [s[0] for s in samples],
-                                                            self.target_q: target_q,
-                                                            self.action: [s[1] for s in samples]
-                                                          })
-
-        if self.total_steps % 1000 == 0:
-          # print loss
-          print l
+    max_q_values = q_values.max(axis=1)
+    # compute target q value
+    target_q = np.array([s.reward + self.gamma*max_q_values[i]*(1-s.done) for i,s in enumerate(batch_steps)])
+    target_q = target_q.reshape([len(batch_steps)])
+    
+    # minimize the TD-error
+    cur_state_batch = [s.cur_step for s in batch_steps]
+    actions = [s.action for s in batch_steps]
+    l, _, = self.sess.run([self.loss, self.train_op], feed_dict={ self.state_input: cur_state_batch,
+                                                                  self.target_q: target_q,
+                                                                  self.action: actions })
+    return l
 
